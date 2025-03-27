a10bee7d5e046d4b76e70206ac23d3edcea56c37
Himanshu02 <91370829+Himanshu0273@users.noreply.github.com>
2025-03-28 01:27:30 +0530

    UC 4: Run until 100, min value is 0

--- Day 2/snake_ladder.py
+++ Day 2/snake_ladder.py
@@ -12,3 +12,27 @@ def rolldie():
 # (0: No Play, 1: Ladder, or 2: Snake)
 def moveToPlay():
     return randint(0,2)
+
+
+#UC 4: Move until you reach 100, the least value the player can have is 0
+def move(pos):
+    movetype = moveToPlay()
+    steps = rolldie()
+    
+    if movetype == 0: #No Play
+        print("You got NO PLAY!!Wait for the next turn")
+    
+    elif movetype == 1: #Ladder
+        print(f"You got Ladder! You move forward {steps} steps.\nStarted From: {pos}")
+        new_pos = pos + steps
+        print(f"Ended at: {pos}")        
+        return new_pos
+    
+    else:  # Snake
+        new_pos = pos - steps
+        if new_pos < 0:
+            print(f"You got Snake! You rolled {steps}, but can't go below 0. Restarting at 0.\n")
+            return 0
+        else:
+            print(f"You got Snake! You move back {steps} steps.\nStarted at: {pos}\nEnded at: {new_pos}")
+            return new_pos
